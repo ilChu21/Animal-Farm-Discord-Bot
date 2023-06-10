5acea03417840b9d99259a65b8e51f374245e8fc
ilchu21 <ilchu21@hotmail.com>
2023-06-09 21:16:02 -0400

    added try/catch

--- utils/utc.js
+++ utils/utc.js
@@ -1,6 +1,10 @@
 export function getNowUtc() {
-  const now = new Date();
-  const currentHour = now.getUTCHours().toString().padStart(2, '0');
-  const currentMinute = now.getUTCMinutes().toString().padStart(2, '0');
-  return `${currentHour}:${currentMinute}`;
+  try {
+    const now = new Date();
+    const currentHour = now.getUTCHours().toString().padStart(2, '0');
+    const currentMinute = now.getUTCMinutes().toString().padStart(2, '0');
+    return `${currentHour}:${currentMinute}`;
+  } catch (error) {
+    console.error(error);
+  }
 }
